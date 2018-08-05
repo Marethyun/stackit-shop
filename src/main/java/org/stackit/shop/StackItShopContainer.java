bb547e434544d01f2e6bafc37307b20b49b8cc24
Misbeliever <bacci.ange@gmail.com>
2018-08-05 23:36:04 +0200

    Added a container object for listeners

---
+++ src/main/java/org/stackit/shop/StackItShopContainer.java
@@ -0,0 +1,10 @@
+package org.stackit.shop;
+
+public abstract class StackItShopContainer {
+
+    protected final StackItShop pluginInstance;
+
+    public StackItShopContainer(StackItShop pluginInstance) {
+        this.pluginInstance = pluginInstance;
+    }
+}
