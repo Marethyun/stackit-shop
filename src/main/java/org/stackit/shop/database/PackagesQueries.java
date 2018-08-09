438fdb419f18657d287eb42951241b5cbd51de86
Misbeliever <bacci.ange@gmail.com>
2018-08-09 22:36:39 +0200

    Added SQL query

--- src/main/java/org/stackit/shop/database/PackagesQueries.java
+++ src/main/java/org/stackit/shop/database/PackagesQueries.java
@@ -3,7 +3,8 @@ package org.stackit.shop.database;
 public enum PackagesQueries {
     GET_ALL("SELECT * FROM %t"),
     INSERT_ONE("INSERT INTO %t (uid, player_uuid, commands, slotsnumber, name, claimed_time) VALUES (:uid, :player_uuid, :commands, :slotsnumber, :name, :claimed_time)"),
-    GET_WITH_PLAYER_UUID("SELECT * FROM %t WHERE player_uuid = :player_uuid");
+    GET_WITH_PLAYER_UUID("SELECT * FROM %t WHERE player_uuid = :player_uuid"),
+    CLAIM_PACKAGE_WITH_ID("UPDATE %t SET claimed_time = :claimed_time WHERE id = :id");
 
     private String query;
 
